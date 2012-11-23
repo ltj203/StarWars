b1f8661f9a2a09f2d11cbb7d09b59b3130979106
Lisa <ljenkin2@hotmail.com>
2012-11-23 13:23:30 -0500

    Initial Commit

---
+++ StarWars/StarWarsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  StarWarsAppDelegate.h
+//  StarWars
+//
+//  Created by Lisa Jenkins on 11/23/12.
+//  Copyright (c) 2012 Lisa Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StarWarsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StarWars/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StarWars
+//
+//  Created by Lisa Jenkins on 11/23/12.
+//  Copyright (c) 2012 Lisa Jenkins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "StarWarsAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([StarWarsAppDelegate class]));
+    }
+}
